63b48df4bc9fcd6a87d191028dfc94bd755fc195
RVishnu17 <fifa14vishnu@gmail.com>
2025-03-02 14:25:12 +0000

    2

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 //import { GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
-const canvas = document.querySelector('canvas.webgl')
+//const canvas = document.querySelector('canvas.webgl')
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.set(0,0,20);
 //camera.up.set(0,0,1);
 //camera.lookAt(0,0,0);
-const renderer = new THREE.WebGLRenderer({canvas:canvas, alpha:true,});
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 //renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
