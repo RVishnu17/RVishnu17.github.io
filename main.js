15dcf29320b94419c9a281a51e60ed01b7235a3d
RVishnu17 <fifa14vishnu@gmail.com>
2025-03-09 13:49:15 +0000

    VII

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.set(0,0,20);
-const renderer = new THREE.WebGLRenderer();
+const canvas = document.querySelector("#c");
+const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-
 
 // Line material
 // Hello World ! 
